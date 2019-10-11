7980f56f92daf359acb5d54094cc5b6fb337897c
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-11 04:24:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,12 +6,12 @@ class Song
     @@all
   end
 
-  def self.save
-    self.class.all << self
+  def save
+    self.all << self
   end
   def self.create
     new_song = self.class.new
-    self.class.all << new_song
+    self.all << new_song
     new_song
   end
 
